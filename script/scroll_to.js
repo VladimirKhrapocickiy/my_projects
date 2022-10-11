bd81f048fcb0019465413cecb130218479e4627d
VladimirKhrapovickiy <vovandossser90@gmail.com>
2022-10-11 17:09:19 +0700

    push try

--- script/scroll_to.js
+++
@@ -1,8 +0,0 @@
-const links = document.querySelectorAll(".scroll_to");
-links.forEach((item) => {
-  item.addEventListener("click", () => {
-    const el = document.getElementById(item.getAttribute("data-link"));
-    console.log(el);
-    el.scrollIntoView({ behavior: "smooth", block: "start" });
-  });
-});
